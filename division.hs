576b20a86debf913d643cb0aac4df60f857872af
Denys Solomennyi <denisonovich@gmail.com>
2025-03-05 23:14:20 +0200

    removed hasAllCourses

--- division.hs
+++ division.hs
@@ -8,26 +8,23 @@ students = ["alice", "bob", "clark", "david", "emily"]
 
 studentCourses :: [(String, String)]
 studentCourses =
-  [ ("alice", "english")
-  , ("alice", "history")
-  , ("bob", "english")
-  , ("bob", "history")
-  , ("bob", "math")
-  , ("clark", "math")
-  , ("emily", "english")
-  , ("emily", "history")
-  , ("emily", "math")
-  , ("emily", "biology")
+  [ ("alice", "english"),
+    ("alice", "history"),
+    ("bob", "english"),
+    ("bob", "history"),
+    ("bob", "math"),
+    ("clark", "math"),
+    ("emily", "english"),
+    ("emily", "history"),
+    ("emily", "math"),
+    ("emily", "biology")
   ]
 
 studentHasCourse :: String -> String -> Bool
 studentHasCourse student course = (student, course) `elem` studentCourses
 
-hasAllCourses :: String -> Bool
-hasAllCourses student = not (any (\course -> not (studentHasCourse student course)) courses)
-
 qualifiedStudents :: [String]
-qualifiedStudents = filter hasAllCourses students
+qualifiedStudents = filter (\student -> all (studentHasCourse student) courses) students
 
 main :: IO ()
 main = do
